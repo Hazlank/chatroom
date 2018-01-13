119833b8e9b919b489ce7ae0e101cf23754fd4a4
e <401840614@qq.com>
2018-01-14 01:36:12 +0800

    update

--- src/components/PublicComponents/Wrapper.vue
+++ src/components/PublicComponents/Wrapper.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="tg-wrapper" v-if="wrapper()" @click='bardisply'>
+  <div class="tg-wrapper" v-if="wrapper" @click='bardisply'>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
 export default {
-  data () {
-    return {
-      ...mapGetters(['wrapper'])
-    }
+  computed: {
+    ...mapGetters(['wrapper'])
   },
   methods: {
     ...mapActions(['bardisply'])
