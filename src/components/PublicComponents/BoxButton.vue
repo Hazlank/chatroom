dbc95a24cb851e36c5fad29d9bdfd82f1689d82f
e <401840614@qq.com>
2018-01-16 23:55:57 +0800

    add box components

---
+++ src/components/PublicComponents/BoxButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <span class="tg-box__button">
+      <button>
+        {{ this.text | toUpperCase }}
+      </button>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'boxbutton',
+  props: ['text'],
+  filters: {
+    toUpperCase (str) {
+      return str.toUpperCase()
+    }
+  }
+}
+</script>
+
---
+++ src/components/PublicComponents/BoxInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="tg-box__input" @click="Infocus" :class="{'is-focus':focusState}">
+    <label>{{ text }}</label>
+    <input ref='input' type="text" @blur="InputFocus" @focus="InputFocus" v-model="value">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'boxinput',
+  props: ['text'],
+  data () {
+    return {
+      value: '',
+      focusState: false
+    }
+  },
+  filters: {
+    toUpperCase (str) {
+      return str.toUpperCase()
+    }
+  },
+  methods: {
+    InputFocus (e) {
+      if (e.type === 'blur') {
+        if (!this.value) {
+          this.focusState = false
+        }
+      } else {
+        if (!this.value) {
+          this.focusState = true
+        }
+      }
+    },
+    Infocus () {
+      this.$nextTick(() => {
+        this.$refs['input'].focus()
+      })
+    }
+  }
+}
+</script>
