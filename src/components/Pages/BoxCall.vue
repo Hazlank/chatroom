dada5c2422733d67e8fd1e9dd034ebe42e40aaef
e <401840614@qq.com>
2018-01-16 23:54:46 +0800

    add pages

---
+++ src/components/Pages/BoxCall.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    call
+  </div>
+</template>
---
+++ src/components/Pages/BoxGroup.vue
@@ -0,0 +1,26 @@
+<!-- type: group -->
+<template>
+  <div class="tg-box__group">
+    <div class="tg-box__groupContent">
+      <box-upload-img></box-upload-img>
+      <box-input :text='"Group name"'></box-input>
+    </div>
+    <footer class="tg-box__groupBottom">
+      <box-button :text='"cancel"'></box-button>
+      <box-button :text='"next"'></box-button>
+    </footer>
+  </div>
+</template>
+
+<script>
+import BoxInput from 'Public/BoxInput'
+import BoxButton from 'Public/BoxButton'
+import BoxUploadImg from 'Public/BoxUploadImg'
+export default {
+  components: {
+    BoxInput,
+    BoxButton,
+    BoxUploadImg
+  }
+}
+</script>
