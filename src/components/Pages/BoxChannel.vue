ad6b5586b426009aeefce3f92b46287f00fb6f1a
e <401840614@qq.com>
2018-01-17 20:50:34 +0800

    add boxChannel

--- src/components/Pages/BoxChannel.vue
+++ src/components/Pages/BoxChannel.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    channel
+  <div class="tg-box__channel">
+    <div class="tg-box__channelContent">
+      <box-uploadimg></box-uploadimg>
+      <div class="tg-box__channelInput">
+        <box-input :text='"Channel name"'></box-input>
+      </div>
+    </div>
+    <div class="tg-box__channelOptional">
+        <box-input :text='"Description (optional)"'></box-input>
+    </div>
+    <footer class="tg-box__channelBottom">
+      <box-button :opreation='"cancel"' :text='"cancel"'></box-button>
+      <box-button :text='"create"'></box-button>
+    </footer>
   </div>
 </template>
+
