de86760f540b9a8b1fa44fc24dd16a6c4ce54dba
e <401840614@qq.com>
2018-01-17 20:50:45 +0800

    add boxGroup

--- src/components/Pages/BoxGroup.vue
+++ src/components/Pages/BoxGroup.vue
@@ -2,25 +2,20 @@
 <template>
   <div class="tg-box__group">
     <div class="tg-box__groupContent">
-      <box-upload-img></box-upload-img>
-      <box-input :text='"Group name"'></box-input>
+      <box-uploadimg></box-uploadimg>
+      <div class="tg-box__groupInput">
+        <box-input :text='"Group name"'></box-input>
+      </div>
     </div>
     <footer class="tg-box__groupBottom">
-      <box-button :text='"cancel"'></box-button>
+      <box-button :opreation='"cancel"' :text='"cancel"'></box-button>
       <box-button :text='"next"'></box-button>
     </footer>
   </div>
 </template>
 
 <script>
-import BoxInput from 'Public/BoxInput'
-import BoxButton from 'Public/BoxButton'
-import BoxUploadImg from 'Public/BoxUploadImg'
 export default {
-  components: {
-    BoxInput,
-    BoxButton,
-    BoxUploadImg
-  }
+  name: 'BoxGroup'
 }
 </script>
