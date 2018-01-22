62e89c426fb43ae3cadef0cde5e7aa98cf996ac0
Hazlank <401840614@qq.com>
2018-01-22 15:17:36 +0800

    Update UserList.vue

--- src/components/Pages/UserList.vue
+++ src/components/Pages/UserList.vue
@@ -1,29 +1,28 @@
 <template>
   <div class="tg-userlist">
     <slot name="loading" v-if='loading'>loading</slot>
-      <div class="tg-user" v-for="(user,index) in userList" 
-        :class="{'active': speekingNum === index,'font-c': speekingNum !== index}"
-        :key="`username-${user.name}`"
-        @click="speekingNumChange(index)"
-        v-if="filterUser(user.name)">
-        <div class="tg-user__avatar">
-          <img :src="user.avatar" alt="头像">
+    <div class="tg-user" v-for="(user,index) in userList" :class="{'active': speekingNum === index}" :key="`username-${user.name}`" @click="talkTo(index, user)" v-if="filterUser(user.name)">
+      <div class="tg-user__avatar">
+        <img :src="user.avatar" alt="头像">
+      </div>
+      <div class="tg-user__block">
+        <div class="tg-block__header">
+          <span class="tg-block__sign" v-if="user.group">组</span>
+          <span class="tg-block__talkto">{{user.name}}</span>
+          <span class="tg-block__staute"></span>
+          <span class="tg-block__time">{{user.lasttime}}</span>
         </div>
-        <div class="tg-user__block">
-          <div class="tg-block__header">
-            <span class="tg-block__sign" v-if="user.group">组</span>
-            <span class="tg-block__talkto">{{user.name}}</span>
-            <span class="tg-block__staute"></span>
-            <span class="tg-block__time">{{user.lasttime}}</span>
-          </div>
-          <div class="tg-block__footer">
+        <div class="tg-block__footer">
+          <div>
             <span class="tg-block__talkname">
-              {{ user.messagecontent[user.messagecontent.length - 1].speaker ? 'You' : user.name | filterSpace }}
-          </span><span class="tg-block__point">:</span>
-            <span class="tg-block__context">{{user.context}}</span>
-            <span v-if="user.unread === 0" class="tg-block__contextcount">{{user.unread}}</span>
+                {{ user.messagecontent.contextItem[user.messagecontent.contextItem.length - 1].speaker ? 'You' : user.name | filterSpace }}
+                <!-- .context[user.messagecontent.length - 1].speaker ? 'You' : user.name | filterSpace -->
+            </span><span class="tg-block__point">:</span>
+            <span class="tg-block__context" v-html="user.context"></span>
           </div>
+          <span v-if="user.messagecontent.unread !== 0" class="tg-block__contextcount">{{user.messagecontent.unread}}</span>
         </div>
+      </div>
     </div>
   </div>
 </template>
@@ -43,7 +42,12 @@
         var reg = new RegExp(`^${this.searchUser}|\\s${this.searchUser}`, 'i')
         return new RegExp(/[\u4e00-\u9fa5]/g).test(this.searchUser) ? reg.test(name) : reg.test(pyfl(name))
       },
-      ...mapActions(['speekingNumChange'])
+      talkTo (index, user) {
+        user.messagecontent.unread = 0
+        this.speekingNumChange(index)
+        this.updateLocalStorage()
+      },
+      ...mapActions(['speekingNumChange', 'updateLocalStorage'])
     },
     computed: {
       ...mapGetters(['speekingNum', 'userList'])
