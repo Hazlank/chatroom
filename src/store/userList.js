2ed6d6d66a726bb76c927f0e09e84484fdfd00f4
Hazlank <401840614@qq.com>
2018-01-22 15:15:32 +0800

    Update userList.js

--- src/store/userList.js
+++ src/store/userList.js
@@ -1,141 +1,144 @@
 export const userlist = [{
-  avatar: '/static/img/avatar0.jpg',
+  avatar: './static/img/avatar0.jpg',
   name: 'hax',
   type: 'group',
   context: '',
-  unread: 200,
+  unread: 1,
   lasttime: '12:15'
 },
 {
-  avatar: '/static/img/avatar1.jpg',
+  avatar: './static/img/avatar1.jpg',
   name: 'nbsaw',
   type: 'group',
   context: '',
-  unread: 200,
+  unread: 1,
   lasttime: '12:15'
 },
 {
-  avatar: '/static/img/avatar2.jpg',
+  avatar: './static/img/avatar2.jpg',
   name: 'lialuo',
   type: 'group',
   context: '',
-  unread: 200,
+  unread: 1,
   lasttime: '12:15'
 },
 {
-  avatar: '/static/img/avatar3.jpg',
+  avatar: './static/img/avatar3.jpg',
   name: 'vipcch',
   type: 'group',
   context: '',
-  unread: 200,
+  unread: 1,
   lasttime: '12:15'
 },
 {
-  avatar: '/static/img/avatar4.jpg',
+  avatar: './static/img/avatar4.jpg',
   name: 'choltte',
   type: 'group',
   context: '',
-  unread: 200,
+  unread: 1,
   lasttime: '12:15'
 },
 {
-  avatar: '/static/img/avatar5.jpg',
+  avatar: './static/img/avatar5.jpg',
   name: 'gwell',
   type: 'group',
   context: '',
-  unread: 200,
+  unread: 1,
   lasttime: '12:15'
 },
 {
-  avatar: '/static/img/avatar6.jpg',
+  avatar: './static/img/avatar6.jpg',
   name: '扑街r v发',
   type: 'group',
   context: '',
-  unread: 200,
+  unread: 1,
   lasttime: '12:15'
 },
 {
-  avatar: '/static/img/avatar7.jpg',
+  avatar: './static/img/avatar7.jpg',
   name: 'haozi',
   type: 'group',
   context: '',
-  unread: 200,
+  unread: 1,
   lasttime: '12:15'
 },
 {
-  avatar: '/static/img/avatar8.jpg',
+  avatar: './static/img/avatar8.jpg',
   name: 'rollup',
   type: 'group',
   context: '',
-  unread: 200,
+  unread: 1,
   lasttime: '12:15'
 },
 {
-  avatar: '/static/img/avatar9.jpg',
+  avatar: './static/img/avatar9.jpg',
   name: 'tj',
   type: 'group',
   context: '',
-  unread: 200,
+  unread: 1,
   lasttime: '12:15'
 },
 {
-  avatar: '/static/img/avatar10.jpg',
+  avatar: './static/img/avatar10.jpg',
   name: 'Evan you',
   type: 'group',
   context: '',
-  unread: 200,
+  unread: 1,
   lasttime: '12:15'
 },
 {
-  avatar: '/static/img/avatar11.jpg',
+  avatar: './static/img/avatar11.jpg',
   name: '阮一峰',
   type: 'group',
   context: '',
-  unread: 200,
+  unread: 1,
   lasttime: '12:15'
 },
 {
-  avatar: '/static/img/avatar12.jpg',
+  avatar: './static/img/avatar12.jpg',
   name: 'Addy Osmani',
   type: 'group',
   context: '',
-  unread: 200,
+  unread: 1,
   lasttime: '12:15'
 },
 {
-  avatar: '/static/img/avatar13.jpg',
+  avatar: './static/img/avatar13.jpg',
   name: 'fimars',
   type: 'group',
   context: '',
-  unread: 200,
+  unread: 1,
   lasttime: '12:15'
 },
 {
-  avatar: '/static/img/avatar14.jpg',
+  avatar: './static/img/avatar14.jpg',
   name: 'Jamie Kyle',
   type: 'group',
   context: '',
-  unread: 200,
+  unread: 1,
   lasttime: '12:15'
 },
 {
-  avatar: '/static/img/avatar15.jpg',
+  avatar: './static/img/avatar15.jpg',
   name: 'Mike Bostock',
   type: 'group',
   context: '',
-  unread: 200,
+  unread: 1,
   lasttime: '12:15'
 }]
 
 export var messagecontent = []
 if (!localStorage.getItem('messagecontent')) {
   messagecontent = messagecontent.concat(userlist.map((d, index) => {
-    return [{
-      text: `i am ${d.name}`,
-      speaker: false,
-      avatar: `/static/img/avatar${index}.jpg`,
-      time: '11:02'
-    }]
+    return {
+      unread: 1,
+      contextItem: [{
+        text: `i am ${d.name}`,
+        speaker: false,
+        avatar: `./static/img/avatar${index}.jpg`,
+        time: '11:02'
+      }]
+    }
   }))
   localStorage.setItem('messagecontent', JSON.stringify(messagecontent))
 } else {
@@ -144,6 +147,6 @@ if (!localStorage.getItem('messagecontent')) {
 
 userlist.forEach((list, index) => {
   var content = messagecontent[index]
-  list['context'] = content[content.length - 1].text
+  list['context'] = content.contextItem[content.contextItem.length - 1].text
   list['messagecontent'] = content
 })
